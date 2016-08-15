166142e3b7f0aecd7387625bb81e4a19f377168a
Shisha <shisha225@gmail.com>
2016-08-15 23:23:29 +0800

    section3 完成

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,10 @@
 window.$ = window.jQuery = require('jquery');
-window.Vue = require('vue');
+// window.Vue = require('vue');
 // require('bootstrap-sass');
 
 
 $(document).ready(function() {
+  // 導覽列向下滾動
   $('a[href^="#"]').click(function(e) {
     e.preventDefault();
     var dest = $(this).attr('href');
@@ -13,5 +14,16 @@ $(document).ready(function() {
         scrollTop: dest.top
       }, 'slow');
     }
+  });
+
+  $('#service').map(function(e) {
+    var $iframe = $(this).find('iframe');
+    $(this).find('.tabs a')
+      .click(function(e) {
+        e.preventDefault();
+        $iframe.attr('src', $(this).attr('href'));
+        $('.actived').removeClass('actived');
+        $(this).parent().addClass('actived');
+      }).first().click();
   });
 });
