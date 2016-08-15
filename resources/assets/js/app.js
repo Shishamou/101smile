c471355c1772b9048e2273236e9f7dd97f088a4d
Shisha <shisha225@gmail.com>
2016-08-15 20:48:04 +0800

    section2 完成

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,17 @@
 window.$ = window.jQuery = require('jquery');
 window.Vue = require('vue');
+// require('bootstrap-sass');
 
-require('bootstrap-sass');
 
 $(document).ready(function() {
-    console.log($.fn.tooltip.Constructor.VERSION);
+  $('a[href^="#"]').click(function(e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    dest = $(dest).offset();
+    if (dest) {
+      $('html,body').animate({
+        scrollTop: dest.top
+      }, 'slow');
+    }
+  });
 });
